25bc49fa922449fb005881dbab70d5e9d6972ae8
kozoma <89560384+kozoma@users.noreply.github.com>
2023-02-03 10:54:39 +0900

    Add files via upload

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <title>순수동물병원 테스트</title>
+    <meta name='author' content='br' />
+    <meta name='keywords' content='순수동물병원' />
+    <meta name='description' content='순수동물병원' />
+    <meta http-equiv="X-UA-Compatible" content="IE=Edge"/>
+    <meta name='viewport' content='width=1180,user-scalable=yes, target-densitydpi=device-dpi' />
+
+    <script src="http://code.jquery.com/jquery-1.11.1.min.js"></script>
+    <script type='text/javascript' src='js/main.js'></script>
+    <script src="https://cdn.jsdelivr.net/npm/swiper/swiper-bundle.min.js"></script>
+    <link rel='stylesheet' href='css/style.css' media='all' />
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@8/swiper-bundle.min.css"/>
+	<link rel="shortcut icon" type="image/x-icon" href="../images/favicon.png">
+
+</head>
+
+<body>
+    <p>hello</p>
+</body>
